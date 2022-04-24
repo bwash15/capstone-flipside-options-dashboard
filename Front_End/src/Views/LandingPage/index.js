ceaa4a678c80ccd3bc83c3a8ef47176b89156853
Naruza <johnpmajor96@gmail.com>
2022-04-24 19:58:12 -0400

    testing query

--- Front_End/src/Views/LandingPage/index.js
+++ Front_End/src/Views/LandingPage/index.js
@@ -1,19 +1,12 @@
 import React from "react";
 
-<<<<<<< HEAD
-function Home() {
-    return(<div>    
-        <h1>Home Page</h1>
-    </div>);
-}
-
-export default Home;
-=======
 function Home(){
+
+    const user = localStorage.getItem("name")
     return(<div>
         <h1>Hello World!!!!!!!!!!!</h1>
+        <h1>{user}</h1>
     </div>);
 }
 
 export default Home
->>>>>>> bf9f207bde6890211f7aa57ebcb5bdc152e6e4eb
