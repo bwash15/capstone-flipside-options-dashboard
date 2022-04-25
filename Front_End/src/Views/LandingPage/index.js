8e537e06f697552c6a274f62f99928b913a76e93
MasterCodeKillr <68078261+MasterCodeKillr@users.noreply.github.com>
2022-04-24 20:09:28 -0400

    logout button

--- Front_End/src/Views/LandingPage/index.js
+++ Front_End/src/Views/LandingPage/index.js
@@ -1,6 +1,12 @@
 import React from "react";
 
 function Home(){
+
+    const logout =()=> {
+        localStorage.clear();
+        window.location.href = '/';
+    };
+
     return(<div>
         <h1>Hello World!!!!!!!!!!!</h1>
         <button onClick={logout}>Logout</button>
