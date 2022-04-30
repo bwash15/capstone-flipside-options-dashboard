d495873736a08c1f02add3af5309668658646579
BWash <btwashington@student.fullsail.edu>
2022-04-30 15:33:49 -0400

    Brought in Basic Express Server Copy with routing to root page and subdir to a 'new-page' and a redirect to an 'old-page'
    Swapped exp_server_copy.js with exp_server.js to use the new server code
    
    Brought in Schemas directory
    ** Each instance is defaulted with 0 for number and '' (EmptyString)     respectively
    > SnapShot.model.js
    > apiPullSchema.js
    > daySchema.js
    > detailsSchema.js
    > greeksSchema.js
    > last_quoteSchema.js
    > oSS_schema.js - Full json format
    > oSS_schema2.js - Embedded Format
    > underlyingAssetSchema.js

--- Back_End/exp_server.js
+++ Back_End/exp_server.js
@@ -1,103 +1,144 @@
 const express = require('express');
 const app = express();
 const path = require('path');
-
-const logServerEvents = require('./logServerEvents');
+const cors = require('cors');
+const corsOptions = require('./_config/corsOptions')
+const {logServerEvents, logger} = require('./_middleware/logServerEvents');
+const errorHandler = require('./_middleware/errorHandler');
 const EventEmitter = require('events');
-const { application } = require('express');
-const { nextTick } = require('process');
 class Emitter extends EventEmitter{};
 const myEmitter = new Emitter();
 
+//*********************************************** */
+//    Schemas
+// var Greek = require('./greeksSchema');
+// //var defGreeks = require('./greeksSchema');
+// // var SnapShotSchema = require('./snapShotSchema');
+// var OptionSnapShot = require('./oSS_schema');
+//*********************************************** */
+//     PORT
 const PORT = process.env.PORT || 3600;
+//*********************************************** */
+//     Logging Middleware
+//   Parameters for logging method
+//   ${req.method} - Type of request (Get, POST, PUT, Delete)
+//   ${req.headers.origin} - what url did the request come from
+//   ${req.url} - what url was requested on our server
+//
+app.use(logger);
+//*********************************************** */
+// myEmitter.emit(`AccessNewPageLogs`, `New Page Access`,'serverActivityLogs','newPageAccessLog.txt');
 
-myEmitter.on('ServerActivityLogs', (msg, path, filename) => logServerEvents(msg, path, filename));
-myEmitter.on('ServerFormDataLogs', (msg, path, filename) => logServerEvents(msg, path, filename));
-myEmitter.on('ServerJSONDataLogs', (msg, path, filename) => logServerEvents(msg, path, filename));
-myEmitter.on('ServerPublicDataLogs', (msg, path, filename) => logServerEvents(msg, path, filename));
-myEmitter.on('AccessIndexPageLogs', (msg, path, filename) => logServerEvents(msg, path, filename));
-myEmitter.on('AccessNewPageLogs', (msg, path, filename) => logServerEvents(msg, path, filename));
-myEmitter.on('AccessHomePageLogs', (msg, path, filename) => logServerEvents(msg, path, filename));
+// myEmitter.on('ServerActivityLogs', (msg, path, filename) => logServerEvents(msg, path, filename));
+// myEmitter.on('ServerFormDataLogs', (msg, path, filename) => logServerEvents(msg, path, filename));
+// myEmitter.on('ServerJSONDataLogs', (msg, path, filename) => logServerEvents(msg, path, filename));
+// myEmitter.on('ServerPublicDataLogs', (msg, path, filename) => logServerEvents(msg, path, filename));
+// myEmitter.on('AccessIndexPageLogs', (msg, path, filename) => logServerEvents(msg, path, filename));
+myEmitter.on('serverError', (msg, path, filename) => logServerEvents(msg, path, filename));
+myEmitter.on('errorLog', (msg, path, filename) => logServerEvents(msg, path, filename));
 myEmitter.on('404pageFoundLogs', (msg, path, filename) => logServerEvents(msg, path, filename));
 
 //*********************************************** */
-// Built-in Middleware
+//     BUILT-IN MIDDLEWARE
+// CORS - Cross Origin Resource Sharing
+// Create directory of Allowed Origins  of domains that will access the application
+app.use(cors(corsOptions));
 
-// Handles Form data when it is submitted
-// URL encoded data = Form data
+//     HANDLES FORM SUBMITTED DATA when it is submitted
+// URL encoded data = Form data > Need this to grab from data when it is submitted
 app.use(express.urlencoded({extended: false}));
-myEmitter.emit(`ServerFormDataLogs`, `Server found form data`,'ServerActivityLogs','formDataLog.txt');
 
-// Handles JSON data when it is submitted
+//     HANDLES JSON SUBMITTED DATA when it is submitted
 app.use(express.json());
-myEmitter.emit(`ServerJSONDataLogs`, `Server found JSON data`,'ServerActivityLogs','jsonDataLog.txt');
 
-// serve static files - searches the public directory for 
-// the request before the other routes
+//***********************************************  */
+//      SERVE STATIC FILES - CSS APPLIED HERE
+// Searches the public directory for the request before the other routes
+
 // Applies the CSS to the application pages
-// > pulls in the Public file
-app.use(express.static(path.join(__dirname, '/public')));
-myEmitter.emit(`ServerPublicDataLogs`, `Server found request in Public directory`,'ServerActivityLogs','publicDataLog.txt');
+// > pulls in the Public file for each route specified
+app.use('/', express.static(path.join(__dirname, '/public')));
+app.use('/subdir', express.static(path.join(__dirname, '/public')));
 
 /************************************************* */
+//      ROUTER-LEVEL MIDDLEWARE
+// Routes to the subdir Route -> then to the index -> then inside subdir to the test file
+app.use('/', require('./_routes/root'));
+app.use('/subdir', require('./_routes/subdir'));
+//app.use('/')
 
-// ** Reads this page like a waterfall **
-// what do we want to do with the page 
-// '/' is the root 
-// res.send('Hello World!');
-// res.sendFile('./views/index.html', {root: __dirname});
-app.get('^/$|/index(.html)?', (req, res) => {
-    myEmitter.emit(`AccessIndexPageLogs`, `Index Page Access`,'ServerActivityLogs','indexPageAccessLog.txt');
-    res.sendFile(path.join(__dirname, 'views', 'index.html'));
-})
-
-/**
+//*************************************************** */
+/**    DATA MODEL PULLS FROM THE DATABASE
+ * 
  * (.html)? - allows someone to find webpage without .html extension on the
  * end of the URL 
  * 
  */
-app.get('/new-page(.html)?', (req, res) => {
-    myEmitter.emit(`AccessNewPageLogs`, `New Page Access`,'ServerActivityLogs','newPageAccessLog.txt');
-    res.sendFile(path.join(__dirname, 'views', 'new-page.html'));
-})
+// app.get('/user/:id', (req, res, next) => {
+//     // if the user ID is 0, skip to the next route
+//     if (req.params.id === '0') next('route')
+//     // otherwise pass the control to the next middleware function in this stack
+//     else next()
+//   }, (req, res, next) => {
+//     // send a regular response
+//     res.send('regular')
+//   })
+  
+//   // handler for the /user/:id path, which sends a special response
+//   app.get('/user/:id', (req, res, next) => {
+//     res.send('special')
+//   })
+//*************************************************** */
 
+
+
+//*************************************************** */
 /**
- *      Redirect from old-page to new-page
- * 
- * > 302 by default, we want a 301 so the browser will 
- * > see the page has been permanently removed
+ *  API PULL FOR OPTIONS *  
  */
-app.get('/old-page(.html)?', (req, res) => {
-    myEmitter.emit(`ServerActivityLogs`, `Old Page Access`,'ServerActivityLogs','oldPageAccessLog.txt');
-    res.redirect(301, '/new-page.html'); 
-})
+// Get all the objects in a collection
+// app.get('/snapShot', function(req,res) {
+//     console.log('getting all options');
+//     Greek.find({}).exec(function(err, results){
+//       if(err){
+//         // log the error
+//         myEmitter.emit('errorLog', 'message: ' + error.message, 'serverErrorLogs', 'server_GetAll_Errors.txt' );
+//         // tell the user of the error
+//         res.send('error has occurred');
+//       }else{
+//         console.log(results);
+//         res.json(results);   
+//       } 
+//     }
+//   )});
+
+/***************************************************** */
 /**
- *    Sending a custom 404 page
+ *    SENDING A 404 PAGE NOT FOUND
+ * 
  * > Express sends a 404 by default
  * > by sending a custom page it will not send a 404 bc it will accually 
- * > send the status 404
- * 
+ * > send the status 404 * 
  */
 
-app.get('/*', (req, res)=> {
-    myEmitter.emit(`404pageFoundLogs`, `404 Page Found`,'ServerActivityLogs','404PageFoundLog.txt');
-    res.status(404).sendFile(path.join(__dirname, 'views', '404.html'));
+app.all('*', (req, res)=> {
+    res.status(404);
+    if(req.accepts('html')){
+        myEmitter.emit(`404pageFoundLogs`, `404 html Page NOT Found`,'serverActivityLogs','html404PageFoundLog.txt');
+        res.sendFile(path.join(__dirname, 'views', '404.html'));
+    }else if (req.accepts('json')){
+        myEmitter.emit(`404pageFoundLogs`, `404 json Page NOT Found`,'serverActivityLogs','json404PageFoundLog.txt');
+        res.json({ error: '404 Not Found'});
+    } else {
+        res.type('txt').send('404 txt Page NOT found');
+    }
 })
 
-// Route Handlers
-app.get('/hell(.html)?', (req, res) => {
-    console.log('attempted to load hello.html');
-    next()
-}, (req, res) => {
-    res.send('Hello World!');
-})
+//****************************************************** */
+//   ERROR HANDLING
+app.use(errorHandler);
+
+//****************************************************** */
 
 app.listen(PORT, () => console.log(`Server Running on ${PORT}`));
 myEmitter.emit(`ServerActivityLogs`, `Server Port: ${PORT}`,'ServerActivityLogs','expServLog.txt');
-
-
-
-
-
-
-
