805eac365431dd51ed5c326a3b3caf6e00485506
MasterCodeKillr <68078261+MasterCodeKillr@users.noreply.github.com>
2022-05-03 12:38:53 -0400

    update 2

--- Front_End/src/index.js
+++ Front_End/src/index.js
@@ -1,14 +1,17 @@
-import React from "react";
-import ReactDOM from "react-dom";
+import {StrictMode} from 'react';
+import {createRoot} from 'react-dom/client';
 import { BrowserRouter } from "react-router-dom";
 import "./index.css";
 import App from "./App";
 
-ReactDOM.render(
-	<React.StrictMode>
+const rootElement = document.getElementById('root');
+const root = createRoot(rootElement);
+
+
+root.render(
+	<StrictMode>
 		<BrowserRouter>
 			<App />
 		</BrowserRouter>
-	</React.StrictMode>,
-	document.getElementById("root")
+	</StrictMode>
 );
