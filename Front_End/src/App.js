f2b2729bf4366a551f5db4d2c3fb5087fe12d8b7
MasterCodeKillr <68078261+MasterCodeKillr@users.noreply.github.com>
2022-05-03 21:35:50 -0400

    Accordian added

--- Front_End/src/App.js
+++ Front_End/src/App.js
@@ -3,9 +3,11 @@ import { isMobile } from "react-device-detect";
 import Main from "./Views/Desktop/LandingPage";
 import Signup from "./Views/Desktop/Signup";
 import Login from "./Views/Desktop/Login";
+import Profile from "./Views/Desktop/ProfilePage";
 import MobileMain from "./Views/Mobile/LandingPage";
 import MobileSignup from "./Views/Mobile/Signup";
 import MobileLogin from "./Views/Mobile/Login";
+import MobileProfile from "./Views/Desktop/ProfilePage";
 
 function App() {
 	const user = localStorage.getItem("token");
@@ -16,7 +18,8 @@ function App() {
 				{user && <Route path="/" exact element={<MobileMain />} />}
 				<Route path="/signup" exact element={<MobileSignup />} />
 				<Route path="/login" exact element={<MobileLogin />} />
-				<Route path="/" element={<Navigate replace to="/login" />} />
+				<Route path="/profile" exact element = {<MobileProfile/>}/>
+				<Route path="/" element={<Navigate replace to="/profile" />} />
 			</Routes>
 		);
 	}
@@ -26,7 +29,8 @@ function App() {
 				{user && <Route path="/" exact element={<Main />} />}
 				<Route path="/signup" exact element={<Signup />} />
 				<Route path="/login" exact element={<Login />} />
-				<Route path="/" element={<Navigate replace to="/login" />} />
+				<Route path="/profile" exact element = {<Profile/>}/>
+				<Route path="/" element={<Navigate replace to="/profile" />} />
 			</Routes>
 		);
 	}
