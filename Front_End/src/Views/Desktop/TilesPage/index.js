2dd92b3a564d4641dcc523034994882d7f0893d0
Naruza <johnpmajor96@gmail.com>
2022-05-10 00:42:09 -0400

    added template for option tile

--- Front_End/src/Views/Desktop/TilesPage/index.js
+++ Front_End/src/Views/Desktop/TilesPage/index.js
@@ -5,37 +5,46 @@ import CardActions from '@mui/material/CardActions';
 import CardContent from '@mui/material/CardContent';
 import Button from '@mui/material/Button';
 import Typography from '@mui/material/Typography';
-
-const bull = (
-  <Box
-    component="span"
-    sx={{ display: 'inline-block', mx: '2px', transform: 'scale(0.8)' }}
-  >
-    •
-  </Box>
-);
+import OptionTile from './OptionTile';
 
 export default function BasicCard() {
   return (
-    <Card sx={{ maxWidth: 275 }}>
-      <CardContent>
-        <Typography sx={{ fontSize: 14 }} color="text.secondary" gutterBottom>
-          Top Stock Option
-        </Typography>
-        <Typography variant="h5" component="div">
-          TQQQ
-        </Typography>
-        <Typography sx={{ mb: 1.5 }} color="text.secondary">
-        ProShares UltraPro QQQ
-        </Typography>
-        <Typography variant="body2">
-            TQQQ provides 3x leveraged exposure to a modified market-cap-weighted index tracking 100 of the largest non-financial firms listed on NASDAQ.
-        </Typography>
-      </CardContent>
-      <CardActions>
-        <Button size="small">Learn More</Button>
-      </CardActions>
-    </Card>
-    
+    <OptionTile/>
   );
 }
+
+
+
+
+
+
+
+
+// const bull = (
+//   <Box
+//     component="span"
+//     sx={{ display: 'inline-block', mx: '2px', transform: 'scale(0.8)' }}
+//   >
+//     •
+//   </Box>
+// );
+
+    // <Card sx={{ maxWidth: 275 }}>
+    //   <CardContent>
+    //     <Typography sx={{ fontSize: 14 }} color="text.secondary" gutterBottom>
+    //       Top Stock Option
+    //     </Typography>
+    //     <Typography variant="h5" component="div">
+    //       TQQQ
+    //     </Typography>
+    //     <Typography sx={{ mb: 1.5 }} color="text.secondary">
+    //     ProShares UltraPro QQQ
+    //     </Typography>
+    //     <Typography variant="body2">
+    //         TQQQ provides 3x leveraged exposure to a modified market-cap-weighted index tracking 100 of the largest non-financial firms listed on NASDAQ.
+    //     </Typography>
+    //   </CardContent>
+    //   <CardActions>
+    //     <Button size="small">Learn More</Button>
+    //   </CardActions>
+    // </Card>
