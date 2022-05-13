09fd80007d024199d7a9c3eb46e83c74825db4c0
BWash <btwashington@student.fullsail.edu>
2022-05-13 08:50:05 -0400

    Updated the cors options to true and resolve the cors error in browser

--- Back_End/_config/corsOptions.js
+++ Back_End/_config/corsOptions.js
@@ -1,7 +1,7 @@
 const allowedOrigins = require('./allowedOrigins');
-const {logServerEvents, logger} = require('../_middleware/logServerEvents');
+const { logServerEvents, logger } = require('../_middleware/logServerEvents');
 const EventEmitter = require('events');
-class Emitter extends EventEmitter{};
+class Emitter extends EventEmitter { };
 const myEmitter = new Emitter();
 myEmitter.on('cors_errors', (msg, path, filename) => logServerEvents(msg, path, filename));
 
@@ -9,7 +9,7 @@ const corsOptions = {
     origin: (origin, callback) => {                  // remove after dev
         if (allowedOrigins.indexOf(origin) !== -1 || !origin) {
             callback(null, true);
-        } else {  
+        } else {
             callback(new Error('Not allowed by CORS'));
         }
     },
