c50f7efe606c7f13aac7d341c7b2b894f3669e49
BWash <btwashington@student.fullsail.edu>
2022-05-25 09:26:12 -0400

    Removed duplicate import of react-router-dom in Home

--- Front_End/src/Pages/LandingPage/index.js
+++ Front_End/src/Pages/LandingPage/index.js
@@ -3,7 +3,7 @@ import React from "react";
 import { useState, useEffect } from "react";
 import { Link, useNavigate, Navigate } from "react-router-dom";
 import { useCustomer, useClient } from "../../context/customer";
-import { Link, useNavigate } from "react-router-dom";
+
 const API_URL = ''
 
 
