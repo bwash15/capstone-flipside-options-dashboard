fbd7f04f0c95072a56d7d5a059ec2db3eb3a1a2f
Dustin <dustinhuntzinger1@gmail.com>
2022-05-26 11:41:21 -0400

    Update route

--- Back_End/exp_server.js
+++ Back_End/exp_server.js
@@ -76,7 +76,7 @@ app.use('/', require('./_routes/root'));
 app.use('/register', require('./_routes/_register'));
 app.use('/auth', require('./_routes/_auth'));
 app.use('/userTiles', require('./_routes/_userTiles'))
-app.use('/ProfilePage', require('./_routes/webapi/_ProfilePage.js'));
+app.use('/ProfilePage', require('./_routes/webapi/_profilePage.js'));
 
 
 //************************************************/
