93a420d2991d35aed9ca880a762f4bad44f985da
Naruza <johnpmajor96@gmail.com>
2022-06-08 23:38:31 -0400

    Finished connecting polygon api to add options page

--- Back_End/_routes/_tiles.js
+++ Back_End/_routes/_tiles.js
@@ -5,6 +5,7 @@ const ROLES_LIST = require('../_config/roles_list');
 const verifyRoles = require('../_middleware/verifyRoles');
 
 
+router.route('/addingOption').post(verifyRoles(ROLES_LIST), TilesController.updateTileInfo);
 router.route('/').post(verifyRoles(ROLES_LIST), TilesController.tileGet)
                  .delete(verifyRoles(ROLES_LIST), TilesController.tileDelete);
 
