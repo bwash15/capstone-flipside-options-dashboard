8864a0d16492c06ec0b6b445286f7f0203e9c31c
Naruza <johnpmajor96@gmail.com>
2022-06-16 03:52:07 -0400

    Fixed Date for option expiration

--- Back_End/_controllers/optionItemController.js
+++ Back_End/_controllers/optionItemController.js
@@ -16,19 +16,12 @@ const updateTileInfo = async (req, res) => {
     const option_expire_date = '220617'                 // YearMonthDay
     const option_ticker = stockName;                       //nasdaq name for the company -> this comapny is called exela but the nasdaq name is XELA
     const option_strike_price = '00' + parseInt(stockPrice) * 1000;             //8 digit number, divide by 1000 -> this will be 1$
-    const options_ticker_link = `O:${option_ticker}${option_expire_date}${option_type}${option_strike_price}`;
-    const multiplier = '1';                             // minutes per minute, hours per hour, days per day, weeks per week -> this will be one hour
-    const timespan = 'hour';                            // minute, hour, day, week, month
-    const from = '2022-06-08';                          //start of the timeframe to look at 
-    const to = '2022-06-08';                            //end of the timeframe to look at
-
-    const api_link =`https://api.polygon.io/v2/aggs/ticker/${options_ticker_link}/range/${multiplier}/${timespan}/${from}/${to}?apiKey=` + process.env.API_KEY;
     const snapshot_link = `https://api.polygon.io/v3/snapshot/options/${option_ticker}/O:${option_ticker}${option_expire_date}${option_type}${option_strike_price}?apiKey=${process.env.API_KEY}`
 
     const response = await fetchOption(snapshot_link);
     const data = await response.json();
     const query = data['results']['day']['close'];
-    
+
     const result = await UserTile.updateOne(
         {uuid: tileUUID},
         {$push: 
